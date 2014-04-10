fe0b75e2e2c5f8be6881485888f6817599a07d43
Toru Furuya <t.furuya825@gmail.com>
2014-04-10 22:43:38 +0900

    modify zsh/alias

--- .zsh/lib/aliases.zsh
+++ .zsh/lib/aliases.zsh
@@ -14,10 +14,19 @@ alias ll="ls -al"
 alias sl=ls # often screw this up
 
 # git
-alias gs='git status'
+alias ga='git add'
+alias gl='git pull'
+alias gp='git push'
 alias gd='git diff --color'
 alias gdc='git diff --cached --color'
+alias gc='git commit'
+alias gca='git commit -a'
 alias gco='git checkout'
+alias gb='git branch'
+alias gs='git status'
+alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
+alias changelog='git log `git log -1 --format=%H -- CHANGELOG*`..; cat CHANGELOG*'
+alias glog="git log --graph --pretty='format:%C(yellow)%h%Cblue%d%Creset %s %C(black bold)%an, %C(red)%ar%Creset'"
 
 # log
 alias plog='tail -f /var/log/apache2/error_log'
